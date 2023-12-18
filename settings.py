0b07e4e5491af50a081104d99a019e73cb6b9f27
zehara <107530847+zehara19@users.noreply.github.com>
2023-12-18 19:48:06 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,3 +3,11 @@ CELERY_TASK_DEFAULT_QUEUE = env("CELERY_TASK_DEFAULT_QUEUE", default="default")
 CELERY_BROKER_TRANSPORT_OPTIONS = {
     "region": env("AWS_REGION", default="us-east-1")
 }
+from datetime import timedelta
+...
+CELERY_BEAT_SCHEDULE = {
+    "beat_task": {
+        "task": "django_aws.tasks.beat_task",
+        "schedule": timedelta(minutes=1),
+    },
+}
